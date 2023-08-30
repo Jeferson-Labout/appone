32893182b715a0cdcb4397bea7ca4c8f3d140530
Jeferson Labout <jefson1989@gmail.com>
2023-08-30 17:50:17 -0300

    commit final

--- lib/main.dart
+++ lib/main.dart
@@ -5,29 +5,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -37,16 +21,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -55,71 +30,96 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  bool _isRunning = false;
+  late DateTime _startTime;
+  late Duration _elapsedTime;
+  late String _timerText;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _elapsedTime = Duration.zero;
+    _timerText = _formatDuration(_elapsedTime);
+  }
+
+  void _toggleTimer() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (_isRunning) {
+        _elapsedTime = DateTime.now().difference(_startTime);
+      } else {
+        _startTime = DateTime.now().subtract(_elapsedTime);
+      }
+      _isRunning = !_isRunning;
+
+      // Iniciar o loop de atualização do cronômetro
+      _updateTimer();
     });
+  }
+
+  void _resetTimer() {
+    setState(() {
+      _elapsedTime = Duration.zero;
+      _timerText = _formatDuration(_elapsedTime);
+    });
+  }
+
+  void _updateTimer() {
+    if (_isRunning) {
+      setState(() {
+        _timerText = _formatDuration(DateTime.now().difference(_startTime));
+      });
+
+      Future.delayed(const Duration(seconds: 1), _updateTimer);
+    }
+  }
+
+  String _formatDuration(Duration duration) {
+    return '${duration.inMinutes.toString().padLeft(2, '0')}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}';
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text(
+              'Cronômetro:',
+              style: TextStyle(fontSize: 20),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              _timerText,
+              style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
             ),
+            const SizedBox(height: 20),
+            if (!_isRunning)
+              ElevatedButton(
+                onPressed: _resetTimer,
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(
+                      255, 41, 13, 145), // Definir a cor vermelha
+                ),
+                child: Text(
+                  'Zerar',
+                  style: TextStyle(
+                      color: Colors.white), // Definir cor do texto como branco
+                ),
+              ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {
+          _toggleTimer();
+        },
+        tooltip: 'Toggle Timer',
+        child: Icon(_isRunning ? Icons.pause : Icons.play_arrow),
+      ),
     );
   }
 }
